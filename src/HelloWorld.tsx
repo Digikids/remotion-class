254b754d9ba2e974addb24da7cdbd7b12146cf38
Max <riungemaina@gmail.com>
2023-10-06 22:00:30 +0300

    feat: added positioning

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,24 +1,47 @@
-import { AbsoluteFill } from 'remotion';
+import { AbsoluteFill } from 'remotion'
 
-import { AnimatedRotation } from '@riunge/kid-motion';
+import { AnimatedHorizontalPosition, AnimatedRotation } from '@riunge/kid-motion'
 
 export const HelloWorld: React.FC = () => {
-	return (
-		<AbsoluteFill style={{ backgroundColor: 'yellow' }}>
-			<AnimatedRotation
-				in={{
-					startTime: 0,
-					endTime: 10,
-				}}
-				out={{
-					startTime: -10,
-					endTime: 0,
-				}}
-				from={180}
-				to={0}
-			>
-				Hello World
-			</AnimatedRotation>
-		</AbsoluteFill>
-	);
-};
+  return (
+    <AbsoluteFill style={{ backgroundColor: 'yellow' }}>
+      <AnimatedHorizontalPosition
+        in={{
+          startTime: 0,
+          endTime: 20,
+        }}
+        out={{
+          startTime: -20,
+          endTime: 0,
+        }}
+        from={0}
+        to={50}
+      >
+        <AnimatedRotation
+          in={{
+            startTime: 0,
+            endTime: 20,
+          }}
+          out={{
+            startTime: -20,
+            endTime: 0,
+          }}
+          from={-180}
+          to={0}
+        >
+          <span
+            style={{
+              display: 'inline-flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              fontFamily: 'cursive',
+              fontSize: 300,
+            }}
+          >
+            Hello World
+          </span>
+        </AnimatedRotation>
+      </AnimatedHorizontalPosition>
+    </AbsoluteFill>
+  )
+}
--- src/Root.tsx
+++ src/Root.tsx
@@ -2,11 +2,20 @@ import { Video } from '@riunge/kid-motion';
 import { HelloWorld } from './helloWorld';
 
 export const RemotionRoot: React.FC = () => (
-	<Video
-		durationInMinutes={1}
-		durationInSeconds={30}
-		format="horizontal"
-		name="HelloWorld"
-		videoSource={HelloWorld}
-	/>
+	<>
+		<Video
+			durationInMinutes={1}
+			durationInSeconds={30}
+			format="horizontal"
+			name="HelloWorld"
+			videoSource={HelloWorld}
+		/>
+		<Video
+			durationInMinutes={1}
+			durationInSeconds={30}
+			format="horizontal"
+			name="HelloWorld2"
+			videoSource={HelloWorld}
+		/>
+	</>
 );
